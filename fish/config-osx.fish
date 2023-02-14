d9b881cf4526d471777c6e91e231355fd8f432a1
sepezho ⠀ <sepezho@sepezhos-MacBook-Pro.local>
2023-02-14 19:53:32 +0100

    add stuff

---
+++ fish/config-osx.fish
@@ -0,0 +1,6 @@
+if type -q exa
+  alias ll "exa -l -g --icons"
+  alias lla "ll -a"
+  alias func "/usr/local/opt/func"
+  alias fift "/usr/local/opt/fift"
+end
---
+++ nvim/init.lua
@@ -0,0 +1,28 @@
+local impatient_ok, impatient = pcall(require, "impatient")
+if impatient_ok then impatient.enable_profile() end
+
+for _, source in ipairs {
+  "core.utils",
+  "core.options",
+  "core.bootstrap",
+  "core.diagnostics",
+  "core.autocmds",
+  "core.mappings",
+  "configs.which-key-register",
+} do
+  local status_ok, fault = pcall(require, source)
+  if not status_ok then vim.api.nvim_err_writeln("Failed to load " .. source .. "\n\n" .. fault) end
+end
+
+astronvim.conditional_func(astronvim.user_plugin_opts("polish", nil, false))
+
+vim.g.copilot_proxy_strict_ssl = false
+
+if vim.fn.has "nvim-0.8" ~= 1 or vim.version().prerelease then
+  vim.schedule(function() astronvim.notify("Unsupported Neovim Version! Please check the requirements", "error") end)
+end
+
+vim.g.NERDTreeShowHidden = 1
+vim.g.copilot_no_tab_map = true
+vim.g.copilot_assume_mapped = true
+vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
