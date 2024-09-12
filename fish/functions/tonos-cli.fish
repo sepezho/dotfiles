93e51cb7eeb54eaae0c2ecb5fcd3ffdc2338808d
sepezho <sepezho@gmail.com>
2024-09-12 10:18:01 +0200

    init

---
+++ fish/functions/tonos-cli.fish
@@ -0,0 +1,4 @@
+function tonos-cli --wraps=/Users/sepezho/Work/local_chain/tonos-cli/target/release/tonos-cli --description 'alias tonos-cli /Users/sepezho/Work/local_chain/tonos-cli/target/release/tonos-cli'
+  /Users/sepezho/Work/local_chain/tonos-cli/target/release/tonos-cli $argv
+        
+end
