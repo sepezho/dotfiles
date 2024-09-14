42deca842b744c16afb79726436ad70d4a66f756
sepezho <sepezho@gmail.com>
2024-09-14 16:33:22 +0200

    fish logremote

--- fish/fish/config-osx.fish
+++
@@ -1,6 +0,0 @@
-if type -q exa
-  alias ll "exa -l -g --icons"
-  alias lla "ll -a"
-  alias func "/usr/local/opt/func"
-  alias fift "/usr/local/opt/fift"
-end
--- fish/fish/functions/tonos-cli.fish
+++
@@ -1,4 +0,0 @@
-function tonos-cli --wraps=/Users/sepezho/Work/local_chain/tonos-cli/target/release/tonos-cli --description 'alias tonos-cli /Users/sepezho/Work/local_chain/tonos-cli/target/release/tonos-cli'
-  /Users/sepezho/Work/local_chain/tonos-cli/target/release/tonos-cli $argv
-        
-end
